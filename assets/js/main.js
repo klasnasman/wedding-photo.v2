d8f4a1c9f13128eae87894d147c599f25d72f133
klasnasman <klas.nasman@gmail.com>
2023-02-24 13:56:57 +0100

    swiper npm people

--- assets/js/main.js
+++ assets/js/main.js
@@ -104,7 +104,7 @@ window.addEventListener('scroll', runCheckScroll);
 const swiper = swiperFunc();
 function swiperFunc() {
   return new Swiper('.swiper', {
-    slidesPerView: 1.2,
+    slidesPerView: 1,
     spaceBetween: 32,
     cssMode: true,
     speed: 800,
